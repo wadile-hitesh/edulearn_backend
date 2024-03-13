49ba5f956de3f6a9f68c752391f0018915ac2597
wadile-hitesh <hiteshwadile.dev@gmail.com>
2024-03-13 21:06:17 +0530

    Mongodb Setup Completed

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+const port = process.env.PORT || 8000;
+
+connectDB().then(()=>{
+    app.on('error', (err) => {
+        console.error('Server error:', err);
+        throw err;
+    });
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}).catch((err)=>{
+    console.log('MongoDb Connection Failed :', err);
+    throw err;
+});
+
